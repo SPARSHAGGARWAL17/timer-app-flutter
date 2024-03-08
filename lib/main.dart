de35a701bcc7fc61e57a4dbe20372fcaf7183028
Sparsh Aggarwal <aggarwalsparsh175@gmail.com>
2024-03-08 14:02:20 +0530

    adds theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:timer_app/navigator/routes.dart';
+import 'package:timer_app/view/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,17 +9,42 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Timer App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Color(0xffB6EAFF),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        buttonTheme: const ButtonThemeData(
+          buttonColor: Color(0xffE1DFFF),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xff006782),
+          centerTitle: false,
+          elevation: 5,
+          shadowColor: Colors.grey,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+          ),
+          foregroundColor: Colors.white,
+          toolbarHeight: 120,
+          // backgroundColor: Color(0xff216C2E),
+        ),
+        cardColor: const Color(0xff0067820D),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xff006782),
+        ),
         useMaterial3: true,
       ),
       onGenerateRoute: AppNamedRoutes.onGenerateRoute,
-      initialRoute: "",
+      initialRoute: HomePage.route,
     );
   }
 }
--- lib/navigator/routes.dart
+++ lib/navigator/routes.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:timer_app/view/home_page.dart';
 
 class AppNamedRoutes {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     late Widget widget;
-    
+
     var routeName = settings.name;
 
     switch (routeName) {
-      case "":
+      case HomePage.route:
+        widget = const HomePage();
         break;
       default:
         widget = const Scaffold();
     }
-    return MaterialPageRoute(builder: (context) => widget,settings: settings);
+    return MaterialPageRoute(builder: (context) => widget, settings: settings);
   }
 }
