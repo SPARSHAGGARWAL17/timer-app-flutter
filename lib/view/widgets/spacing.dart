985875dfdf68cd4e7133e58d3c40f59b9192b0d3
Sparsh Aggarwal <aggarwalsparsh175@gmail.com>
2024-03-08 14:24:51 +0530

    create a common methods to add spacing in between widgets

---
+++ lib/view/widgets/spacing.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+SizedBox vSpacing([double height = 10]) {
+  return SizedBox(height: height);
+}
+
+SizedBox hSpacing([double width = 10]) {
+  return SizedBox(width: width);
+}
